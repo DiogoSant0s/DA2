08f483abaedb2612e18d2c4adb81ba73a0552181
DiogoSant0s <up202009291@up.pt>
2023-06-04 22:46:34 +0100

    Final

--- docs/output/html/search/functions_3.js
+++ docs/output/html/search/functions_3.js
@@ -6,7 +6,7 @@ var searchData=
   ['gethasname_3',['getHasName',['../class_data.html#aecf5bcdd6b05d64accdf4518daaa8c6b',1,'Data']]],
   ['getnodes_4',['getNodes',['../class_graph.html#a4e09ba677d50defaab9a4157c11c8a82',1,'Graph']]],
   ['getrealgraph_5',['getRealGraph',['../class_data.html#a251d0f3917eede5a505ea0a4ca36a9c5',1,'Data']]],
-  ['gettourdistance_6',['getTourDistance',['../class_graph.html#a472f38fba0a902a6b35796a917cc5ca9',1,'Graph']]],
+  ['gettourdistance_6',['getTourDistance',['../class_graph.html#a27a7ee0705c15e22cdb58a6d57191f84',1,'Graph']]],
   ['getuserinput_7',['getUserInput',['../class_menu.html#a5ddb2e9403391596669bba5f64bea959',1,'Menu']]],
   ['graph_8',['Graph',['../class_graph.html#ae4c72b8ac4d693c49800a4c7e273654f',1,'Graph']]]
 ];
